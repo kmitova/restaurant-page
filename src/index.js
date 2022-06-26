33df78a998818d37c5801b569343d973400c8b93
kmitova <96522051+kmitova@users.noreply.github.com>
2022-06-26 15:49:20 +0300

    static element logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import _ from 'lodash';
-// import "./style.css";
+import "./style.css";
 
 // function component() {
 //   const element = document.createElement("div");
@@ -12,5 +12,22 @@
 // }
 
 // document.body.appendChild(component());
-
+const content = document.getElementById('content')
 console.log('now in the index.js file')
+const topSection = document.createElement('div')
+const heading = document.createElement('h1')
+heading.textContent = 'Restaurant Name'
+topSection.appendChild(heading)
+content.appendChild(topSection)
+const listItems = document.createElement('ul')
+content.appendChild(listItems)
+const home = document.createElement('li')
+home.textContent = 'Home'
+listItems.appendChild(home)
+const menu = document.createElement("li");
+menu.textContent = "Menu";
+listItems.appendChild(menu);
+const contact = document.createElement("li");
+contact.textContent = "Contact";
+listItems.appendChild(contact);
+
